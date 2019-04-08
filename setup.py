3fdedc20b2f432244032b70bb1754b184863a3fa
Guillaume Fieni <gfieni@users.noreply.github.com>
2019-04-08 11:56:20 +0200

    build: Fix usage of the wrong library in setup script

--- setup.py
+++ setup.py
@@ -14,5 +14,5 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from distutils.core import setup
+from setuptools import setup
 setup()
