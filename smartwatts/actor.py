1f971029b414429df19b942d7cf4303cf1a8c585
Guillaume Fieni <gfieni@users.noreply.github.com>
2019-05-14 15:45:45 +0200

    feat: Use formula State to store the runtime configuration of the formula

--- smartwatts/actor.py
+++ smartwatts/actor.py
@@ -14,10 +14,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from __future__ import annotations
+
 import logging
 import re
+from typing import Dict
 
-from powerapi.formula import FormulaActor
+from powerapi.formula import FormulaActor, FormulaState
 from powerapi.handler import PoisonPillMessageHandler
 from powerapi.message import PoisonPillMessage
 from powerapi.pusher import PusherActor
@@ -27,12 +30,39 @@ from smartwatts.handler import ReportHandler, FormulaScope
 from smartwatts.topology import CPUTopology
 
 
+class SmartWattsFormulaConfig:
+    """
+    Global config of the SmartWatts formula.
+    """
+
+    def __init__(self, scope: FormulaScope, rapl_event: str, error_threshold: float, cpu_topology: CPUTopology):
+        self.scope = scope
+        self.rapl_event = rapl_event
+        self.error_threshold = error_threshold
+        self.cpu_topology = cpu_topology
+
+
+class SmartWattsFormulaState(FormulaState):
+    """
+    State of the SmartWatts formula actor.
+    """
+
+    def __init__(self, actor: SmartWattsFormulaActor, pushers: Dict[str, PusherActor], config: SmartWattsFormulaConfig):
+        FormulaState.__init__(self, actor, pushers)
+        self.config = config
+
+        m = re.search(r'^\(\'(.*)\', \'(.*)\', \'(.*)\'\)$', actor.name)  # TODO: Need a better way to get these information
+        self.dispatcher = m.group(1)
+        self.sensor = m.group(2)
+        self.socket = m.group(3)
+
+
 class SmartWattsFormulaActor(FormulaActor):
     """
     This actor handle the reports for the SmartWatts formula.
     """
 
-    def __init__(self, name, power_report_pusher: PusherActor, formula_report_pusher: PusherActor, scope: FormulaScope, rapl_event: str, error_threshold: float, cpu_topology: CPUTopology):
+    def __init__(self, name: str, pushers: Dict[str, PusherActor], config: SmartWattsFormulaConfig):
         """
         Initialize a new actor.
         :param name: Name of the actor
@@ -43,24 +73,13 @@ class SmartWattsFormulaActor(FormulaActor):
         :param error_threshold: Error threshold triggering the learning
         :param cpu_topology: CPU topology to use
         """
-        FormulaActor.__init__(self, name, power_report_pusher, logging.WARNING)
-        self.scope = scope
-        self.rapl_event = rapl_event
-        self.error_threshold = error_threshold
-        self.formula_report_pusher = formula_report_pusher
-        self.cpu_topology = cpu_topology
-
-        m = re.search(r'^\(\'(.*)\', \'(.*)\', \'(.*)\'\)$', name)  # TODO: Need a better way to get these information
-        self.dispatcher = m.group(1)
-        self.sensor = m.group(2)
-        self.socket = m.group(3)
+        FormulaActor.__init__(self, name, pushers, logging.WARNING)
+        self.state: SmartWattsFormulaState = SmartWattsFormulaState(self, pushers, config)
 
     def setup(self):
         """
-        Setup the actor.
+        Setup the messages handlers.
         """
         FormulaActor.setup(self)
-        self.add_handler(PoisonPillMessage, PoisonPillMessageHandler())
-        self.formula_report_pusher.connect_data()
-        handler = ReportHandler(self.sensor, self.actor_pusher, self.formula_report_pusher, self.socket, self.scope, self.rapl_event, self.error_threshold, self.cpu_topology)
-        self.add_handler(HWPCReport, handler)
+        self.add_handler(PoisonPillMessage, PoisonPillMessageHandler(self.state))
+        self.add_handler(HWPCReport, ReportHandler(self.state))
