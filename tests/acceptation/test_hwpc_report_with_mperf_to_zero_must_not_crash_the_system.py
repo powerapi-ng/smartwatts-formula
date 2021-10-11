74d6d10e6df49d015d4d68218537afa084b567b1
Lauric Desauw <lauric.desauw@inria.fr>
2021-10-11 10:59:02 +0200

    fix: update pylint rule

--- tests/acceptation/test_hwpc_report_with_mperf_to_zero_must_not_crash_the_system.py
+++ tests/acceptation/test_hwpc_report_with_mperf_to_zero_must_not_crash_the_system.py
@@ -81,7 +81,7 @@ def test_normal_behaviour(mongo_database, shutdown_system):
               'cpu-ratio-base': 19,
               'cpu-ratio-max': 42,
               'cpu-error-threshold': 2.0,
-              'sensor-reports-frequency': 1000,
+              'sensor-report-sampling-interval': 1000,
               'learn-min-samples-required': 10,
               'learn-history-window-size': 60,
               'real-time-mode': False}
