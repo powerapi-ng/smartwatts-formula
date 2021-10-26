bbe4310f9c9392b8c45e8970d6f3aa0a593038a4
Pierre Rust <pierre.rust@orange.com>
2021-10-26 16:47:52 +0200

    Fix quotes and model in config generation

--- auto_config.sh
+++ auto_config.sh
@@ -7,30 +7,30 @@ basefrequency=$(expr ${basefrequency}\*1000 | bc | cut -d . -f 1)
 
 echo "
 {
-  "verbose": true,
-  "stream": true,
-  "input": {
-    "puller": {
-      "model": "HWPCReport",
-      "type": "socket",
-      "uri": "127.0.0.1",
-      "port": 8080,
-      "collection": "test_hwpc"
+  \"verbose\": true,
+  \"stream\": true,
+  \"input\": {
+    \"puller\": {
+      \"model\": \"HWPCReport\",
+      \"type\": \"socket\",
+      \"uri\": \"127.0.0.1\",
+      \"port\": 8080,
+      \"collection\": \"test_hwpc\"
     }
   },
-  "output": {
-    "pusher_power": {
-      "type": "mongodb",
-      "uri": "mongodb://127.0.0.1",
-      "db": "test",
-      "collection": "prep"
+  \"output\": {
+    \"pusher_power\": {
+      \"type\": \"mongodb\",
+      \"uri\": \"mongodb://127.0.0.1\",
+      \"db\": \"test\",
+      \"collection\": \"prep\"
     }
   },
-  "cpu-ratio-base": $basefrequency,
-  "cpu-ratio-min": $minfrequency,
-  "cpu-ratio-max": $maxfrequency,
-  "cpu-error-threshold": 2.0,
-  "disable-dram-formula": true,
-  "sensor-report-sampling-interval": 1000
+  \"cpu-frequency-base\": $basefrequency,
+  \"cpu-frequency-min\": $minfrequency,
+  \"cpu-frequency-max\": $maxfrequency,
+  \"cpu-error-threshold\": 2.0,
+  \"disable-dram-formula\": true,
+  \"sensor-report-sampling-interval\": 1000
 }
 " > ./config_file.json
