2e68e4a99a15d07a31414c3bfbd4a6e25ba6ea78
Guillaume Fieni <guillaume.fieni@inria.fr>
2023-03-29 16:45:08 +0200

    refactor: Cleanup HwPC report handler

--- smartwatts/handler/hwpc_report.py
+++ smartwatts/handler/hwpc_report.py
@@ -26,149 +26,28 @@
 # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-from collections import deque, OrderedDict, defaultdict
-from hashlib import sha1
-from math import ldexp, fabs
-from pickle import dumps
 
-from powerapi.formula import FormulaPoisonPillMessageHandler
-from powerapi.formula.abstract_cpu_dram_formula import AbstractCpuDramFormulaState
+import logging
+from collections import OrderedDict, defaultdict
+from math import ldexp, fabs
+
 from powerapi.handler import Handler
 from powerapi.report import PowerReport, HWPCReport
 from sklearn.exceptions import NotFittedError
-from sklearn.linear_model import ElasticNet as Regression
 
+from smartwatts.model.power_model import PowerModel
 from smartwatts.report import FormulaReport
 
 
-class History:
+class HwPCReportHandler(Handler):
     """
-    This class stores the reports history to use when learning a new power model.
-    """
-
-    def __init__(self, max_length):
-        """
-        Initialize a new reports history container.
-        :param max_length: Maximum amount of samples to keep before overriding the oldest sample at insertion
-        """
-        self.max_length = max_length
-        self.X = deque(maxlen=max_length)
-        self.y = deque(maxlen=max_length)
-
-    def __len__(self):
-        """
-        Compute the length of the history.
-        :return: Length of the history
-        """
-        return len(self.X)
-
-    def store_report(self, power_reference, events_value):
-        """
-        Append a report to the reports history.
-        :param events_value: List of raw events value
-        :param power_reference: Power reference corresponding to the events value
-        """
-        self.X.append(events_value)
-        self.y.append(power_reference)
-
-
-class PowerModel:
-    """
-    This Power model compute the power estimations and handle the learning of a new model when needed.
+    HwPC reports handler.
     """
 
-    def __init__(self, frequency, history_window_size):
-        """
-        Initialize a new power model.
-        :param frequency: Frequency of the power model
-        :param history_window_size: Size of the history window used to keep samples to learn from
-        """
-        self.frequency = frequency
-        self.model = Regression()
-        self.hash = 'uninitialized'
-        self.history = History(history_window_size)
-        self.id = 0
-
-    def learn_power_model(self, min_samples, min_intercept, max_intercept):
-        """
-        Learn a new power model using the stored reports and update the formula id/hash.
-        :param min_samples: Minimum amount of samples required to learn the power model
-        :param min_intercept: Minimum value allowed for the intercept of the model
-        :param max_intercept: Maximum value allowed for the intercept of the model
-        """
-        if len(self.history) < min_samples:
-            return
-
-        fit_intercept = len(self.history) == self.history.max_length
-        model = Regression(fit_intercept=fit_intercept, positive=True).fit(self.history.X, self.history.y)
-
-        # Discard the new model when the intercept is not in specified range
-        if not min_intercept <= model.intercept_ < max_intercept:
-            return
-
-        self.model = model
-        self.hash = sha1(dumps(self.model)).hexdigest()
-        self.id += 1
-
-    @staticmethod
-    def _extract_events_value(events):
-        """
-        Creates and return a list of events value from the events group.
-        :param events: Events group
-        :return: List containing the events value sorted by event name
-        """
-        return [value for _, value in sorted(events.items())]
-
-    def store_report_in_history(self, power_reference, events):
-        """
-        Store the events group into the System reports list and learn a new power model.
-        :param power_reference: Power reference (in Watt)
-        :param events: Events value
-        """
-        self.history.store_report(power_reference, self._extract_events_value(events))
-
-    def compute_power_estimation(self, events):
-        """
-        Compute a power estimation from the events value using the power model.
-        :param events: Events value
-        :raise: NotFittedError when the model haven't been fitted
-        :return: Power estimation for the given events value
-        """
-        return self.model.predict([self._extract_events_value(events)])[0]
-
-    def cap_power_estimation(self, raw_target_power, raw_global_power):
-        """
-        Cap target's power estimation to the global power estimation.
-        :param raw_target_power: Target power estimation from the power model (in Watt)
-        :param raw_global_power: Global power estimation from the power model (in Watt)
-        :return: Capped power estimation (in Watt) with its ratio over global power consumption
-        """
-        target_power = raw_target_power - self.model.intercept_
-        global_power = raw_global_power - self.model.intercept_
-
-        ratio = target_power / global_power if global_power > 0.0 and target_power > 0.0 else 0.0
-        power = target_power if target_power > 0.0 else 0.0
-        return power, ratio
-
-    def apply_intercept_share(self, target_power, target_ratio):
-        """
-        Apply the target's share of intercept from its ratio from the global power consumption.
-        :param target_power: Target power estimation (in Watt)
-        :param target_ratio: Target ratio over the global power consumption
-        :return: Target power estimation including intercept (in Watt) and ratio over global power consumption
-        """
-        intercept = target_ratio * self.model.intercept_
-        return target_power + intercept
-
-
-class ReportHandler(Handler):
-    """
-    Handler behaviour for HWPC Reports
-    """
-
-    def __init__(self, state: AbstractCpuDramFormulaState):
-        Handler.__init__(self, state=state)
+    def __init__(self, state):
+        Handler.__init__(self, state)
         self.models = self._gen_models_dict()
+        self.ticks = {}
 
     def _gen_models_dict(self):
         """
@@ -183,7 +62,7 @@ class ReportHandler(Handler):
         """
         Find and returns the nearest frequency layer for the given frequency.
         :param frequency: CPU frequency
-        :return: Nearest frequency layer for the given frequency
+        :return: The nearest frequency layer for the given frequency
         """
         last_layer_freq = 0
         for current_layer_freq in self.models.keys():
@@ -215,41 +94,28 @@ class ReportHandler(Handler):
          Process a HWPC report and send the result(s) to a pusher actor.
          :param message: Received message
         """
-        self.state.actor.logger.debug('received message ' + str(message))
-        self.state.ticks.setdefault(message.timestamp, {}).update({message.target: message})
+        logging.debug('received message: %s', message)
+        self.ticks.setdefault(message.timestamp, {}).update({message.target: message})
 
-        # start to process the oldest tick only after receiving at least 5 ticks.
-        # we wait before processing the ticks in order to mitigate the possible delay of the sensor/database.
-        if self.state.config.real_time_mode:
-            if len(self.state.ticks) > 2:
-                power_reports, formula_reports = self._process_oldest_tick()
-                for report in power_reports:
-                    for name, pusher in self.state.pushers.items():
-                        pusher.send_data(report)
-                        self.state.actor.logger.debug('send ' + str(report) + ' to ' + name)
-                for report in formula_reports:
-                    for name, pusher in self.state.formula_pushers.items():
-                        pusher.send_data(report)
-                        self.state.actor.logger.debug('send ' + str(report) + ' to ' + name)
-
-        else:
-            if len(self.state.ticks) > 5:
-                power_reports, formula_reports = self._process_oldest_tick()
-                for report in power_reports:
-                    for name, pusher in self.state.pushers.items():
-                        pusher.send_data(report)
-                        self.state.actor.logger.debug('send ' + str(report) + ' to ' + name)
-                for report in formula_reports:
-                    for name, pusher in self.state.formula_pushers.items():
-                        pusher.send_data(report)
-                        self.state.actor.logger.debug('send ' + str(report) + ' to ' + name)
+        # Start to process the oldest tick only after receiving at least 5 ticks.
+        # We wait before processing the ticks in order to mitigate the possible delay between the sensor/database.
+        if len(self.ticks) > 5:
+            power_reports, formula_reports = self._process_oldest_tick()
+            for report in power_reports:
+                for name, pusher in self.state.pushers.items():
+                    pusher.send_data(report)
+                    logging.debug('send power report: %s to %s', report, name)
+            for report in formula_reports:
+                for name, pusher in self.state.formula_pushers.items():
+                    pusher.send_data(report)
+                    logging.debug('send formula report: %s to %s', report, name)
 
     def _process_oldest_tick(self):
         """
         Process the oldest tick stored in the stack and generate power reports for the running target(s).
         :return: Power reports of the running target(s)
         """
-        timestamp, hwpc_reports = self.state.ticks.popitem(last=False)
+        timestamp, hwpc_reports = self.ticks.popitem()
 
         # reports of the current tick
         power_reports = []
@@ -261,6 +127,7 @@ class ReportHandler(Handler):
         except KeyError:
             # cannot process this tick without the reference measurements
             return power_reports, formula_reports
+
         rapl = self._gen_rapl_events_group(global_report)
         avg_msr = self._gen_msr_events_group(global_report)
         global_core = self._gen_agg_core_report_from_running_targets(hwpc_reports)
@@ -411,14 +278,3 @@ class ReportHandler(Handler):
                 agg_core_events_group[event_name] += event_value
 
         return agg_core_events_group
-
-
-class SmartWattsFormulaPoisonPillMessageHandler(FormulaPoisonPillMessageHandler):
-    """
-    Smartwatts Handler for dealing with PoisonPillMessage
-    """
-
-    def teardown(self, soft=False):
-        FormulaPoisonPillMessageHandler.teardown(self, soft=soft)
-        for pusher in self.state.formula_pushers:
-            pusher.socket_interface.close()
