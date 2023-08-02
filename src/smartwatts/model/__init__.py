5b30a8cc6bcc9212677a310aae5d58d9c8d8983e
Guillaume Fieni <guillaume.fieni@inria.fr>
2023-08-02 21:31:45 +0200

    chore: Use the same license format in every files

--- src/smartwatts/model/__init__.py
+++ src/smartwatts/model/__init__.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2022, INRIA
+# BSD 3-Clause License
+#
+# Copyright (c) 2022, Inria
 # Copyright (c) 2022, University of Lille
 # All rights reserved.
 #
--- src/smartwatts/model/report_history.py
+++ src/smartwatts/model/report_history.py
@@ -1,4 +1,6 @@
-# Copyright (c) 2023, INRIA
+# BSD 3-Clause License
+#
+# Copyright (c) 2023, Inria
 # Copyright (c) 2023, University of Lille
 # All rights reserved.
 #
