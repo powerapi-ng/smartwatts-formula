7aea16a81a1c3ae47fceb664e3d4e1a51bf9f89f
Guillaume Fieni <guillaume.fieni@inria.fr>
2023-10-26 16:09:24 +0200

    feat(model): Add Error History support to Frequency Layer

--- src/smartwatts/model/frequency_layer.py
+++ src/smartwatts/model/frequency_layer.py
@@ -31,7 +31,7 @@
 
 from typing import List
 
-from .sample_history import ReportHistory
+from .sample_history import ReportHistory, ErrorHistory
 from .power_model import PowerModel
 
 
@@ -40,14 +40,16 @@ class FrequencyLayer:
     Frequency layer of the CPU.
     """
 
-    def __init__(self, frequency: int, min_samples: int, samples_window_size: int):
+    def __init__(self, frequency: int, min_samples: int, samples_window_size: int, error_window_size: int) -> None:
         """
         Initialize a new frequency layer.
         :param min_samples: Minimum amount of samples required before trying to learn a power model
         :param samples_window_size: Size of the samples history window used to keep samples to learn from
+        :param error_window_size: Size of the error history window used to keep errors of the model
         """
         self.model = PowerModel(frequency, min_samples)
         self.samples_history = ReportHistory(samples_window_size)
+        self.error_history = ErrorHistory(error_window_size)
 
     def update_power_model(self, min_intercept: float, max_intercept: float) -> None:
         """
@@ -64,3 +66,10 @@ class FrequencyLayer:
         :param events_value: Events value (Hardware Performance Counters) of the target
         """
         self.samples_history.store_report(power_reference, events_value)
+
+    def store_error_in_history(self, error: float) -> None:
+        """
+        Append an error to the history.
+        :param error: Power model error
+        """
+        self.error_history.store_error(error)
