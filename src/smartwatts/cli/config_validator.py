8d6c87e4d381719a907088e39cd0a91e42120b1a
Guillaume Fieni <guillaume.fieni@inria.fr>
2023-11-20 16:23:56 +0100

    fix(cli): Wrong prefix Error History parameters name

--- src/smartwatts/cli/config_validator.py
+++ src/smartwatts/cli/config_validator.py
@@ -59,8 +59,8 @@ class SmartWattsConfigValidator(ConfigValidator):
         if config['learn-min-samples-required'] < 0 or config['learn-history-window-size'] < 0:
             raise InvalidConfigurationParameterException('Report history parameters must be positive')
 
-        if config['error-window-size'] < 0:
+        if config['learn-error-window-size'] < 0:
             raise InvalidConfigurationParameterException('Error history window size must be positive')
 
-        if config['error-window-method'] not in ['mean', 'median']:
+        if config['learn-error-window-method'] not in ['mean', 'median']:
             raise InvalidConfigurationParameterException('Error window method is not supported')
